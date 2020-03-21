34e8ea6d378853c968e603f15f316a6303ccfce2
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-22 01:53:25 +0530

    Update power of three.cpp

--- power of three.cpp
+++ power of three.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)
+        if(n<=0)
             return false;
-        if(n<0){
-            return false;
-        }
-            else
         return (1162261467%n)==0;
     }
 };
