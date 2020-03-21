328cfda7bdeb9807b575c74c1c0024af50bd2d3e
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-22 04:26:44 +0530

    Create find_the_duplicate_number.cpp

---
+++ find_the_duplicate_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++)m[nums[i]]++;
+        for(auto i:m){
+            if(i.second>=2)
+                return i.first;
+        }
+        return -1;
+    }
+};
