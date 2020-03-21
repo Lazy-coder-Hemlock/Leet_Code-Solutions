a5ec73ea3549079f19bfd7441d1c8afb9d4390d6
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-22 01:52:26 +0530

    Create power of three.cpp

---
+++ power of three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        if(n<0){
+            return false;
+        }
+            else
+        return (1162261467%n)==0;
+    }
+};
