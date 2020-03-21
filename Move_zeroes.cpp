0bd137a127eedb3969e066b694b17ddaacebe2d4
Mayank singh Pathania <39670439+Hemlock420@users.noreply.github.com>
2020-03-21 22:17:13 +0530

    Create Move_zeroes.cpp

---
+++ Move_zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    int index=0;
+        for(int i=0;i<nums.size();i++){
+        if(nums[i])
+            nums[index++]=nums[i];
+    }
+        while(index<nums.size())
+            nums[index++]=0;
+    }
+};
