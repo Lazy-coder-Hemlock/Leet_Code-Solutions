39285800128ce81232946270d112ffedab49af8d
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-23 05:51:50 +0530

    Create 204. Count Primes.cpp

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        bool arr[n+1];
+        memset(arr,true,sizeof(arr));
+        for(int i=2;i*i<=n;i++)
+        {
+            if(arr[i]==true){
+                for(int j=i*i;j<=n;j+=i){
+                    arr[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+        if(arr[i])
+            count++;}
+        return count;
+    }
+};
