fbc882df1df6bda1ca93e4f21e35272df4308647
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-25 07:32:30 +0530

    Create 74. Search a 2D Matrix.cpp

---
+++ 74. Search a 2D Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       if(matrix.size()==0)
+           return false;
+        int i=0;
+        int j=matrix[0].size()-1;
+        while(i<matrix.size() and j>=0){
+            if(matrix[i][j]==target)
+                return true;
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+        
+    }
+};
