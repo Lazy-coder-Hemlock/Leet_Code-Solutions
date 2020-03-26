7622b857125f836a24bdb1a9e8b2b6842d959ca4
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-26 05:35:28 +0530

    Create 260. Single Number III.cpp

---
+++ 260. Single Number III.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+    map<int,int>m;vector<int>v;
+        for(int i=0;i<nums.size();i++)m[nums[i]]++;
+        for(auto i:m){
+            if(i.second==1)
+                v.push_back(i.first);
+        }
+        return v;
+    }
+};
