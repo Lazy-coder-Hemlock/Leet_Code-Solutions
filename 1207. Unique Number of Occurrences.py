1b142c68bb073c2d9f32c611126628aa19a8bd71
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-03-31 15:52:05 +0530

    Create 1207. Unique Number of Occurrences.py

---
+++ 1207. Unique Number of Occurrences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d={}
+        for i in arr:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        k=[i for i in d.values()]
+        return len(k)==len(set(k))
