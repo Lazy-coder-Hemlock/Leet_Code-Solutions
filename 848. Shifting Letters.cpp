9ac83e13db03f38179235e221a368216e71ccbca
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-04-19 03:37:34 +0530

    Create 848. Shifting Letters.cpp

---
+++ 848. Shifting Letters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string shiftingLetters(string S, vector<int>& shifts) {
+  for(int i=shifts.size()-2;i>=0;i--){
+      shifts[i]=(shifts[i]+shifts[i+1])%26;
+  }
+        for(int i=0;i<shifts.size();i++)
+            S[i]=((S[i]-'a'+shifts[i])%26+'a');      
+    return S;
+        
+        }
+};
