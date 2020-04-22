ab00836de2213ca7077bb77b69bad5332e9a14f9
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-04-22 08:22:13 +0530

    Create 107. Binary Tree Level Order Traversal II.cpp

---
+++ 107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(!root)
+            return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int len=q.size();
+            vector<int>v;
+            for(int i=0;i<len;i++){
+                auto node=q.front();
+                q.pop();
+                v.push_back(node->val);
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            ans.push_back(v);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
