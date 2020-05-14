69a350f2846d5eb40b6c450378d66a850680972d
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-05-14 17:51:34 +0530

    Create 1302. Deepest Leaves Sum.py

---
+++ 1302. Deepest Leaves Sum.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        queue=[]
+        queue.append(root)
+        l=[]
+        while queue:
+            size=len(queue)
+            temp=[]
+            for i in range(size):
+                node=queue.pop(0)
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        l.append(temp)
+        res=l[-1]
+        return sum(res)
+            
+        
