4fe61d3c9e9a4c8fea499ed2928b1a29c3a6b5e5
Mayank singh Pathania <39670439+Lazy-coder-Hemlock@users.noreply.github.com>
2020-06-10 20:34:16 +0530

    Create 386. Lexicographical Numbers.py

---
+++ 386. Lexicographical Numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        l=[]
+        for i in range(1,n+1):
+            l.append(str(i))
+        l.sort()
+        return l
+        
